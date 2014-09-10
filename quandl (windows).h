8df33be3f181d05247e78eb82533f2b209191109
Zhiwei <z_a_fu@hotmail.com>
2014-09-10 18:30:48 +0100

    Update quandl (windows).h
    1. The GET method is put in a more safe style with the host added;
    2. The content of response header is ignored when written to the file.

--- quandl (windows).h
+++ quandl (windows).h
@@ -1,7 +1,7 @@
 /*
  * quandl.h
  *
- *  Created on: 14/11/2013 (Updated on 07/09/2014)
+ *  Created on: 14/11/2013 (Updated on 10/09/2014)
  *      Author: Zhiwei Fu
  */
 
@@ -196,7 +196,8 @@ class quandl {
 			}
 
 			// To send the request.
-			request += website + "\r\n";
+			request += website + " HTTP/1.1\r\nHost: "
+				+ host + "\r\nConnection: close\r\n\r\n";
 			//write(sockfd,request.c_str(),request.length());
 			send(sockfd, request.c_str(), request.length(), 0);
 
@@ -210,11 +211,29 @@ class quandl {
 			// To read dataflow for the file.
 			//memset(&message, 0, iMessage);
 			int iRecv = 1; iStart = 1;
-			string TempStr;
+			//string TempStr;
 			//char tChar[1];
+			int nLoop = 0;
 			while (iRecv != 0 && iRecv != -1){
+				nLoop++;
 				iRecv = recv(sockfd, message, iMessage, MSG_WAITALL);
-				fid.write(message, iRecv);
+				if (nLoop == 1) {
+					string TempStr;
+					TempStr = string(message);
+					string::size_type iStart = TempStr.find("\r\n\r\n");
+					if (iStart < iRecv - 4) {
+						int nLenth = iRecv;
+						TempStr = TempStr.substr(iStart + 4, nLenth - (iStart + 4));
+						nLenth = TempStr.length();
+						fid.write(TempStr.c_str(), nLenth);
+					}
+					else {
+						fid.write(message, iRecv);
+					}
+				}
+				else {
+					fid.write(message, iRecv);
+				}
 			}
 
 				// To close the downloaded file
